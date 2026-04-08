06aeeb78588010e5e22f6c191ce9189ffa85f9a7
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-08 13:17:28 -0300

    feat: add attributes on EventDto

--- src/main/java/dev/devdreamer/Event/infrastructure/dto/EventDto.java
+++ src/main/java/dev/devdreamer/Event/infrastructure/dto/EventDto.java
@@ -1,6 +1,18 @@
 package dev.devdreamer.Event.infrastructure.dto;
 
-public record EventDto(
+import dev.devdreamer.Event.core.enums.EventType;
 
+import java.time.LocalDateTime;
+
+public record EventDto(
+        String name,
+        String description,
+        String identifier,
+        String local,
+        String host,
+        String capacity,
+        EventType eventType,
+        LocalDateTime start,
+        LocalDateTime end
 ) {
 }
