76c2cccce5017e9c1054d7eaa1229e0b08509af8
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-08 12:48:40 -0300

    feat: add Event Mapper (domain to dto and dto to domain )

--- src/main/java/dev/devdreamer/Event/infrastructure/Mapper/EventMapper.java
+++ src/main/java/dev/devdreamer/Event/infrastructure/Mapper/EventMapper.java
@@ -1,4 +1,13 @@
 package dev.devdreamer.Event.infrastructure.Mapper;
 
-public class EventMapper {
+import dev.devdreamer.Event.core.domain.Event;
+import dev.devdreamer.Event.infrastructure.dto.EventDto;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = "spring")
+public interface EventMapper {
+    Event toDomain (EventDto dto);
+    EventDto toDto (Event domain);
 }
