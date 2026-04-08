3d86824e1e82a8708ffcc0e5252bc12134a4b403
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-08 12:54:09 -0300

    feat: call the create method implementing the EventGateway interface

--- src/main/java/dev/devdreamer/Event/infrastructure/gateway/EventResitoryGateway.java
+++ src/main/java/dev/devdreamer/Event/infrastructure/gateway/EventResitoryGateway.java
@@ -1,4 +1,11 @@
 package dev.devdreamer.Event.infrastructure.gateway;
 
-public class EventResitoryGateway {
+import dev.devdreamer.Event.core.domain.Event;
+import dev.devdreamer.Event.core.gateway.EventGateway;
+
+public class EventResitoryGateway implements EventGateway {
+    @Override
+    public Event createEvent(Event event) {
+        return null;
+    }
 }
