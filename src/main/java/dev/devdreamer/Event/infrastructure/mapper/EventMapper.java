7b20dffe64eb8fae4fef1706589c5fe57fb69c1a
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-10 18:37:55 -0300

    refactor: change the Commercial letter on mapper folder and on the FindEventUseCaseImpl, add an new UseCaseBeanConfig it will give contexto to the springboot

--- src/main/java/dev/devdreamer/Event/infrastructure/mapper/EventMapper.java
+++ src/main/java/dev/devdreamer/Event/infrastructure/mapper/EventMapper.java
@@ -1,13 +1,18 @@
-package dev.devdreamer.Event.infrastructure.Mapper;
+package dev.devdreamer.Event.core.mapper;
 
 import dev.devdreamer.Event.core.domain.Event;
 import dev.devdreamer.Event.infrastructure.dto.EventDto;
+import dev.devdreamer.Event.infrastructure.persistence.EventEntity;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 @Component
 @Mapper(componentModel = "spring")
 public interface EventMapper {
+    //---------------PRESENTATION------------------//
     Event toDomain (EventDto dto);
     EventDto toDto (Event domain);
+    //---------------REPOSITORY GATEWAY------------------//
+    EventEntity toEntity (Event Event);
+    Event toDomain (EventEntity Entity);
 }
