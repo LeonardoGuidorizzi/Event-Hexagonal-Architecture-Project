a4f9ae08160d981b0803db5fd0921bf976462482
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-13 11:23:12 -0300

    feat: add execute method from the FindEventUseCase Interface

--- src/main/java/dev/devdreamer/Event/core/usecases/FindEventUseCaseImpl.java
+++ src/main/java/dev/devdreamer/Event/core/usecases/FindEventUseCaseImpl.java
@@ -1,12 +1,18 @@
 package dev.devdreamer.Event.core.usecases;
 
 import dev.devdreamer.Event.core.domain.Event;
+import dev.devdreamer.Event.core.gateway.EventGateway;
 
 import java.util.List;
 
-public class findEventUseCaseImpl implements FindEventUseCase {
+public class FindEventUseCaseImpl implements FindEventUseCase {
+    private final EventGateway eventGateway;
+    public FindEventUseCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
     @Override
     public List<Event> execute() {
-        return List.of();
+        return eventGateway.findEvent();
     }
 }
