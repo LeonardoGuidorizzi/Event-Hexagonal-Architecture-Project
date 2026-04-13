b353262c307e20dcc78f44b3c9061b976ac71a51
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-13 11:20:26 -0300

    feat: add methods findall and create method on EventyRepositoryGateway basically it's where's the businnes rule and sql manipulation

--- src/main/java/dev/devdreamer/Event/infrastructure/gateway/EventResitoryGateway.java
+++ src/main/java/dev/devdreamer/Event/infrastructure/gateway/EventResitoryGateway.java
@@ -2,10 +2,34 @@ package dev.devdreamer.Event.infrastructure.gateway;
 
 import dev.devdreamer.Event.core.domain.Event;
 import dev.devdreamer.Event.core.gateway.EventGateway;
+import dev.devdreamer.Event.infrastructure.mapper.EventMapper;
+import dev.devdreamer.Event.infrastructure.persistence.EventEntity;
+import dev.devdreamer.Event.infrastructure.persistence.EventRepository;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public class EventResitoryGateway implements EventGateway {
+    private final EventRepository repository;
+    private final EventMapper mapper;
+
+    public EventResitoryGateway(EventRepository repository, EventMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
     @Override
     public Event createEvent(Event event) {
-        return null;
+        EventEntity entity = mapper.toEntity(event);
+        return mapper.toDomain(repository.save(entity));
     }
+
+    @Override
+    public List<Event> findEvent() {
+        List<EventEntity> entities = repository.findAll();
+        return entities.stream().map(mapper::toDomain).toList();
+    }
+
+
 }
