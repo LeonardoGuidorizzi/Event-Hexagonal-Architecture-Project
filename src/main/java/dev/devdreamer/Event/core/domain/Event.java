604724f2c5fec082632b55638aeefd09362a319a
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-13 10:49:55 -0300

    refactor: change id type and capacity type

--- src/main/java/dev/devdreamer/Event/core/domain/Event.java
+++ src/main/java/dev/devdreamer/Event/core/domain/Event.java
@@ -5,13 +5,13 @@ import dev.devdreamer.Event.core.enums.EventType;
 import java.time.LocalDateTime;
 
 public record Event(
-        String id,
+        Long id,
         String name,
         String description,
         String identifier,
         String local,
         String host,
-        String capacity,
+        Integer capacity,
         EventType eventType,
         LocalDateTime start,
         LocalDateTime end,
