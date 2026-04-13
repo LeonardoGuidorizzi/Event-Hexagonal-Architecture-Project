ae01e0c04ce7712727806abc1e447c19ad0aa573
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-04-13 11:13:10 -0300

    feat: add comment and gateway to add the create method and the event argument

--- src/main/java/dev/devdreamer/Event/core/usecases/CreateEventUseCaseImpl.java
+++ src/main/java/dev/devdreamer/Event/core/usecases/CreateEventUseCaseImpl.java
@@ -1,10 +1,18 @@
 package dev.devdreamer.Event.core.usecases;
 
 import dev.devdreamer.Event.core.domain.Event;
-
+import dev.devdreamer.Event.core.gateway.EventGateway;
+//i could add context using the @Service annotation here, but i'd be breaking the clean arch rule
+//that's why i used a @Bean annotation in the infrastructure
 public class CreateEventUseCaseImpl implements CreateEventUseCase{
+    private final EventGateway eventGateway;
+
+    public CreateEventUseCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
     @Override
     public Event execute(Event event) {
-        return null;
+        return eventGateway.createEvent(event);
     }
 }
